1d7b860c7517f64e657ccb3cf53eaf91cb886243
sehee <jaydensehee@gmail.com>
2019-04-02 09:34:38 +0900

    remove faceboom comments

--- src/components/Post/Author.js
+++ src/components/Post/Author.js
@@ -1,20 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import config from "../../../content/meta/config";
-
 const Author = props => {
   const { note, theme } = props;
 
   return (
     <React.Fragment>
       <div className="author">
-        {/*<div className="avatar">*/}
-        {/*  <img*/}
-        {/*    src={config.gravatarImgMd5 == "" ? avatar : config.gravatarImgMd5}*/}
-        {/*    alt={config.siteTitle}*/}
-        {/*  />*/}
-        {/*</div>*/}
         <div className="note" dangerouslySetInnerHTML={{ __html: note }} />
       </div>
 
