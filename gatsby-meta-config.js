50e78c768944082d5fb50f8eea905ddfdb52da14
chad-l <chad.l@kakaoenterprise.com>
2022-07-01 12:23:50 +0900

    favicon 변경

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: `Sehee Lee`,
   description: `Sehee Lee's Blog`,
   author: `Sehee Lee(Chad Lee)`,
-  introduction: `Search Service Developer in Kakao.`,
+  introduction: `Search Service Developer in Kakao Enterprise.`,
   siteUrl: `http://seheelee.com`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
@@ -12,7 +12,7 @@ module.exports = {
     instagram: `seheelee___`,
     email: `chadsehee@gmail.com`
   },
-  icon: `content/assets/felog.png`, // Add your favicon
+  icon: `content/assets/favicon.png`, // Add your favicon
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
@@ -27,5 +27,5 @@ module.exports = {
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
   },
-  ga: '', // Add your google analytics tranking ID
+  ga: 'test', // Add your google analytics tranking ID
 }
